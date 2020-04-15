e32d86686ee4cac2b42866dfb4359c334962a15f
ugur ciftci <ugurprog@gmail.com>
2020-04-15 13:26:17 +0300

    spring boot project created

---
+++ kolaybpm/src/main/java/com/project/kolaybpm/KolaybpmApplication.java
@@ -0,0 +1,13 @@
+package com.project.kolaybpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolaybpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KolaybpmApplication.class, args);
+	}
+
+}
---
+++ kolaybpm/src/test/java/com/project/kolaybpm/KolaybpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kolaybpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolaybpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
