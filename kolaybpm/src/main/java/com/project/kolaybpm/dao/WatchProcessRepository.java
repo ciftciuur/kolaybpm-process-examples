284ac9f49a608be8a678cdaa0348257e786a8f0e
ugur ciftci <ugurprog@gmail.com>
2020-06-01 20:39:45 +0300

    remove watch process model and repository
    but activiti have watch process and server port change to  : 7070

--- kolaybpm/src/main/java/com/project/kolaybpm/dao/WatchProcessRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.kolaybpm.dao;
-
-import com.project.kolaybpm.model.WatchProcess;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WatchProcessRepository extends CrudRepository<WatchProcess, Long> {
-}
