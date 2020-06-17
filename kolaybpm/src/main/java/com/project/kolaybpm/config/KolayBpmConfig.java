89693adba7ea27cc253f9f6d2de5afb85e95913f
ugur ciftci <ugurprog@gmail.com>
2020-06-17 23:32:37 +0300

    remove all test packages
    and next step added integretion test package and class

--- kolaybpm/src/main/java/com/project/kolaybpm/config/KolayBpmConfig.java
+++ kolaybpm/src/main/java/com/project/kolaybpm/config/KolayBpmConfig.java
@@ -4,33 +4,33 @@ import org.activiti.engine.*;
 import org.activiti.spring.ProcessEngineFactoryBean;
 import org.activiti.spring.SpringProcessEngineConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
-@ComponentScan
 public class KolayBpmConfig {
-
     /*
     TODO: spring boot environment adında bir interface aracılığıyla application.properties üzerinde bulunan her değişkeni okuyabiliyoruz , örnegin  env.getProperty("app.name") gibi
      */
 
-    @Autowired
-    private Environment environment;
 
     @Bean
     public SpringProcessEngineConfiguration processEngineConfiguration() {
         SpringProcessEngineConfiguration config = new SpringProcessEngineConfiguration();
         config.setDataSource(databaseConnection());
         config.setTransactionManager(transactionManager());
-        config.setDatabaseSchemaUpdate("create");
+        //TODO : ilk kurulumda " create " olucak tabloların olusması için sonra none kalıcak
+        config.setDatabaseSchemaUpdate("none");
         config.setHistory("audit");
         config.setAsyncExecutorActivate(true);
         return config;
@@ -53,8 +53,8 @@ public class KolayBpmConfig {
     //TODO : database bilgilerini buradan değilde application.properties dosyasından okumalı
     @Bean
     public DataSource databaseConnection() {
-        return DataSourceBuilder.create().url(environment.getProperty("spring.datasource.url")).username(environment.getProperty("spring.datasource.username"))
-                .password(environment.getProperty("spring.datasource.password")).driverClassName(environment.getProperty("spring.datasource.driver-class-name")).build();
+        return DataSourceBuilder.create().url("jdbc:postgresql://localhost:5432/kolaybpm").username("postgres")
+                .password("postgres").driverClassName("org.postgresql.Driver").build();
     }
 
     @Bean
